cfc59f04c1a5379c5379d8781bf8c6ba7e29736c
damyks <damianojeh@gmail.com>
2025-06-15 14:56:57 +0100

    deleted some test files

--- backend/editor/views.py
+++ backend/editor/views.py
@@ -1,9 +1,8 @@
-# editor/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CodeExecutionSerializer
-from .tasks import execute_code  # Celery task
+from .tasks import execute_code
 
 
 class CodeExecutionView(APIView):
@@ -12,10 +11,8 @@ class CodeExecutionView(APIView):
         if serializer.is_valid():
             code = serializer.validated_data["code"]
             language = serializer.validated_data["language"]
-            # Trigger async task for code execution
-            print("langusge:  ", language)
+            # create  async task for code execution
             task = execute_code.delay(code, language)
-            # For simplicity, wait for the result (in production, use polling or websockets)
             result = task.get(timeout=40)
             print("result", result)
             return Response({"output": result}, status=status.HTTP_200_OK)
